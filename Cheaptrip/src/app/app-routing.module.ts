e4736d664e4b4e8c08c82bcf05cefd5011dba43e
Celestinvib <celestinvib@gmail.com>
2022-09-16 21:13:58 +0200

    AdminCity routerlinks v0.1

--- Cheaptrip/src/app/app-routing.module.ts
+++ Cheaptrip/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CityAddComponent } from './components/admin/city/city-add/city-add.component';
+import { CitiesListComponent } from './components/admin/city/cities-list/cities-list.component';
+
 
 const routes: Routes = [
   {
     path:'cities/add',
     component: CityAddComponent
+  },
+  {
+    path:'cities',
+    component: CitiesListComponent
   }
+
 ];
 
 @NgModule({
