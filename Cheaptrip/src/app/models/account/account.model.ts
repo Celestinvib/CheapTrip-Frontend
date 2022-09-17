60d784fd6816490a3a1e089df541176a5921bb16
Celestinvib <celestinvib@gmail.com>
2022-09-18 00:58:02 +0200

    JWT security v1.0

---
+++ Cheaptrip/src/app/models/account/account.model.ts
@@ -0,0 +1,11 @@
+export class Account {
+  id?:any;
+  name?:string;
+  surnames?:string;
+  email?:string;
+  password?:string;
+  phone_number?:string;
+  birth_date?:Date;
+  creation_date?:Date;
+  status?:number;
+}
