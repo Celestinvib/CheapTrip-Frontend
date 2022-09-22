2fe0790d3691a7280107884985b4a5c8edeaa42a
pbesora <pau_besora@hotmail.com>
2022-09-22 12:13:13 +0200

    Bargains-accounts model and service

---
+++ Cheaptrip/src/app/models/bargains-accounts/bargains-accounts.model.spec.ts
@@ -0,0 +1,7 @@
+import { BargainsAccounts } from './bargains-accounts.model';
+
+describe('BargainsAccounts', () => {
+  it('should create an instance', () => {
+    expect(new BargainsAccounts()).toBeTruthy();
+  });
+});
