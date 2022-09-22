8d1a790b0dc92b7aecc4cf81ed384787ea831622
pbesora <pau_besora@hotmail.com>
2022-09-22 14:26:21 +0200

    Filters now use db data

--- Cheaptrip/src/app/components/main/filters/filters.component.ts
+++ Cheaptrip/src/app/components/main/filters/filters.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FeatureService } from "../../../services/feature/feature.service";
+import { CityService } from "../../../services/city/city.service";
+import { City } from "../../../models/city/city.model";
+import { Feature } from "../../../models/feature/feature.model";
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  cities: City[] = [];
+  features: Feature[] = [];
 
-  constructor() { }
+  constructor(private featureService: FeatureService, private cityService: CityService) { }
 
   ngOnInit(): void {
+    this.getFeatures();
+    this.getCities();
   }
 
+  getFeatures() {
+    this.featureService.getAll()
+      .subscribe(
+        (result) => {
+          this.features = result;
+
+        },
+        (error) => {
+          console.log('Was impossible to take features info');
+        }
+      );
+  }
+
+  getCities() {
+    this.cityService.getAll()
+      .subscribe(
+        (result) => {
+          this.cities = result;
+
+        },
+        (error) => {
+          console.log('Was impossible to take cities info');
+        }
+      );
+  }
 }
